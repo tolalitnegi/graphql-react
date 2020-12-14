a37cf3064dc1cac9d5b0de974705f75cef9bb91d
Lalit Negi <tolalitnegi@gmail.com>
2020-12-14 17:13:17 +0000

    firebase integration

--- src/App.js
+++ src/App.js
@@ -7,11 +7,29 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 import SignInOut from './pages/sign-in-out/sign-in-out.jsx';
+import {auth} from './firebase/firebase.util';
 
-function App() {
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {currentUser: null}
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => this.setState({currentUser: user}));
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
   return (
     <div>
-      <Header />
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -19,6 +37,7 @@ function App() {
       </Switch>
     </div>
   );
+  }
 }
 
 export default App;
